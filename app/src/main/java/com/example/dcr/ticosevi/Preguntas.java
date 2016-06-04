239b7d91691b2016a889786554b409e7f87db18c
nashyra <nashyralb@gmail.com>
2016-06-03 21:45:28 -0600

    commit

--- app/src/main/java/com/example/dcr/ticosevi/Preguntas.java
+++ app/src/main/java/com/example/dcr/ticosevi/Preguntas.java
@@ -24,19 +24,35 @@ public class Preguntas {
 
     public void cargarPrueba(){
         Pregunta pre1 = new Pregunta();
-        Pregunta pre2 = new Pregunta(0, "Significa aumentar la contaminación de nuestro planeta, el consumo de combustible y atentar contra la salud:\n", "", "Comprobar el buen estado de los dispositivos del sistema de control de emisiones del motor.\n",
-                "La alteración de los elementos de control de emisiones del motor.\n", "Conservar en el mejor estado o sustituir los componentes del sistema de control de emisiones del motor.\n", "La alteración de los elementos de control de emisiones del motor.");
+        Pregunta pre2 = new Pregunta(0, "Significa aumentar la contaminación de nuestro planeta, el consumo de combustible y atentar contra la salud:\n",
+                "", "Comprobar el buen estado de los dispositivos del sistema de control de emisiones del motor.\n",
+                "La alteración de los elementos de control de emisiones del motor.\n",
+                "Conservar en el mejor estado o sustituir los componentes del sistema de control de emisiones del motor.\n",
+                "La alteración de los elementos de control de emisiones del motor.");
         Pregunta pre3 = new Pregunta(1, "¿Es posible adelantar en una rotonda?", "", "Falso.",
                 "Verdadero.", "", "Falso.");
         Pregunta pre4 = new Pregunta(2, "La imagen que se muestra corresponde a una señal de:", "limitelargo", "Información.",
                 "Reglamentación.", "Prevención.", "Reglamentación.");
         Pregunta pre5 = new Pregunta(3, "Facilita el control de aceleración y potencia del motor dentro del rango que se determina como conducción económica y eficiente:",
                 "", "El medidor de velocidad.", "El medidor de temperatura.", "El tacómetro.", "El tacómetro.");
+        Pregunta pre6 = new Pregunta(4, "Cuál es un componente del sistema de escape?:",
+                "", "Catalizador.", "Carburador.", "Bobina.", "Catalizador.");
+        Pregunta pre7 = new Pregunta(5, "Se suspenderá la licencia a aquel conductor que en categoría común o profesional:",
+                "", "Acumule 6 puntos.", "Acumule 12 puntos o más.", "Acumule 6 puntos o más.", "Acumule 12 puntos o más.");
+        Pregunta pre8 = new Pregunta(6, "En las curvas:", "", "Durante la noche utilizar la luz alta para tener mayor visibilidad.",
+                "Nunca se debe adelantar y se debe mantener el mismo carril.", "Se debe mantener la velocidad con la que se ingresó.",
+                "Nunca se debe adelantar y se debe mantener el mismo carril.");
+        Pregunta pre9 = new Pregunta(7, "Además de aire y calor(chispa), que más se necesita para que el motor funcione:", "",
+                "Agua.", "Aceite.", "Combustible.", "Combustible.");
         this.preguntas.add(pre1);
         this.preguntas.add(pre2);
         this.preguntas.add(pre3);
         this.preguntas.add(pre4);
         this.preguntas.add(pre5);
+        this.preguntas.add(pre6);
+        this.preguntas.add(pre7);
+        this.preguntas.add(pre8);
+        this.preguntas.add(pre9);
 
     }
 
