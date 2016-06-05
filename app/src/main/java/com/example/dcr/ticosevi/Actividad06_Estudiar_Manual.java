4a6210a1a259887077e096991fad5f956d5ae191
depm1992 <depm100792@gmail.com>
2016-06-05 02:24:26 -0600

    estudiar manual

--- app/src/main/java/com/example/dcr/ticosevi/Actividad06_Estudiar_Manual.java
+++ app/src/main/java/com/example/dcr/ticosevi/Actividad06_Estudiar_Manual.java
@@ -2,12 +2,13 @@ package com.example.dcr.ticosevi;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
 public class Actividad06_Estudiar_Manual extends Base {
 
-    static int pos = 1;
+    static int pos = 0;
     static String[] imagenes = new String[224];
 
     @Override
@@ -15,6 +16,8 @@ public class Actividad06_Estudiar_Manual extends Base {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad06__estudiar__manual);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         cargarImangenes();
 
@@ -22,10 +25,11 @@ public class Actividad06_Estudiar_Manual extends Base {
             @Override
             public void onClick(View v) {
                 pos--;
-                if(pos != 0){
+                if (pos != 0) {
                     ImageView iVPre = (ImageView) findViewById(R.id.iVEstudiarManual);
                     String aux = imagenes[pos];
                     int res_imagen = getResources().getIdentifier(aux, "drawable", getPackageName());
+                    iVPre.setImageResource(res_imagen);
                 }
             }
         });
@@ -34,10 +38,11 @@ public class Actividad06_Estudiar_Manual extends Base {
             @Override
             public void onClick(View v) {
                 pos++;
-                if(pos != 224){
+                if (pos != 224) {
                     ImageView iVPre = (ImageView) findViewById(R.id.iVEstudiarManual);
                     String aux = imagenes[pos];
                     int res_imagen = getResources().getIdentifier(aux, "drawable", getPackageName());
+                    iVPre.setImageResource(res_imagen);
                 }
             }
         });
@@ -50,9 +55,8 @@ public class Actividad06_Estudiar_Manual extends Base {
         });
     }
 
-    public void cargarImangenes(){
+    public void cargarImangenes() {
         imagenes[0] = "a1";
         imagenes[1] = "a2";
     }
-
 }
