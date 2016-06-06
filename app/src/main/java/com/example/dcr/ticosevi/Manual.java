0b03d82d6e85ce5fcc811c41b62bce25574f0b40
nashyra <nashyralb@gmail.com>
2016-06-05 20:24:52 -0600

    commit2

--- app/src/main/java/com/example/dcr/ticosevi/Manual.java
+++ app/src/main/java/com/example/dcr/ticosevi/Manual.java
@@ -1,8 +1,10 @@
 package com.example.dcr.ticosevi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Manual extends Base {
@@ -17,20 +19,41 @@ public class Manual extends Base {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        cargarImagenes();
+        cargarImangenes();
 
-        findViewById(R.id.btnsiguiente).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnanterior).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Mensaje(pos + "");
+                pos--;
+                if (pos != 0) {
+                    ImageView iVPre = (ImageView) findViewById(R.id.imageView3);
+                    String aux = imagenes.get(pos);
+                    Mensaje(imagenes.get(pos));
+                    int res_imagen = getResources().getIdentifier(aux, "drawable", getPackageName());
+                    iVPre.setImageResource(res_imagen);
+                }
+            }
+        });
+
+        Button btnsiguiente = (Button) findViewById(R.id.btnsiguiente);
+        //Programamos el evento onclick
+        btnsiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
                 next();
+            }
+        });
+
+        findViewById(R.id.btnregresar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Manual.this, Actividad02.class));
             }
         });
     }
 
     public void next(){
         //pos++;
-
         if (pos != 224) {
             ImageView iVPre = (ImageView) findViewById(R.id.imageView3);
             String aux = "a";
@@ -44,9 +67,10 @@ public class Manual extends Base {
         }
     }
 
-    public void cargarImagenes() {
-        imagenes[0] = "a1";
-        imagenes[1] = "a2";
+    public void cargarImangenes() {
+        imagenes.add("a");
+        imagenes.add("a1");
+        imagenes.add("a2");
     }
 
 }
